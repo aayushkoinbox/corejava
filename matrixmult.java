97e6739cd62a8c28d3c89aec0503050ce60acdc9
aayushkoinbox <aayushkoinbox@gmail.com>
2025-07-09 15:56:27 +0545

    add matrixmult

---
+++ matrixmult.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class matrixmult{
+public static void main(String... args)
+{
+Scanner input = new Scanner(System.in);
+
+String name;
+
+System.out.println("Name: Sudeep");
+
+name = input.nextLine();
+
+
+
+}
+
+}
