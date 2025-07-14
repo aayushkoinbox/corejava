7f74490127643c44b594934291cf4160781d8821
aayushkoinbox <aayushkoinbox@gmail.com>
2025-07-14 11:54:54 +0545

    package folder proj for area

---
+++ proj/geoarea.java
@@ -0,0 +1,12 @@
+import meropackage.area;
+import java.util.Scanner;
+
+public class geoarea{
+public static void main(String... args){
+ area a = new area();
+ a.rectangle(5,10);
+    a.circle(20);
+a.square(9);
+}
+
+}
---
+++ proj/meropackage/area.java
@@ -0,0 +1,22 @@
+package meropackage;
+
+public class area{
+
+
+public void rectangle(int l, int b){
+
+ System.out.println("Area of rectangle: "+ l*b );
+}
+
+public void circle (double r){
+
+System.out.println("Area of cirle: "+ 3.14*r*r);
+}
+
+public void square (int l){
+
+System.out.println("Area of square: "+ l*l);
+
+}
+
+}
